6b49dae7bc9107cdf76f000ac94cfd14dbb6e053
liichao <zijuechao@gmail.com>
2020-12-10 22:06:00 +0800

    添加tools文件夹

---
+++ config/tools/README.md
@@ -0,0 +1,20 @@
+### 文件列表and下载地址
+tools工具包中的文件与下载地址
+放到/tmp/k8s/tools目录中即可使用
+#### cfssl
+https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+
+#### cfssljson
+https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+
+#### hyperkube1.16.15
+https://storage.googleapis.com/kubernetes-release/release/v1.16.15/bin/linux/amd64/hyperkube
+
+#### ETCD下载地址。
+curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+
+https://github.com/etcd-io/etcd/releases/download/v3.4.14/etcd-v3.4.14-linux-amd64.tar.gz
+
+#### docker 二进制文件下载
+18.09.6
+https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VER}.tgz
---
+++ tools/tools.go
@@ -0,0 +1,39 @@
+package tools
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/op/go-logging"
+)
+
+// 定义日志格式
+var log = logging.MustGetLogger("example")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} > %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+// GetIPDes 获取IP前面的地址
+func GetIPDes(ips string) (string, int, int) {
+	if len(strings.Split(ips, `.`)) != 4 {
+		return "", 1, 1
+	}
+	if len(strings.Split(ips, `-`)) != 2 {
+		result := strings.Split(ips, `.`)[0] + "." + strings.Split(ips, `.`)[1] + "." + strings.Split(ips, `.`)[2] + "."
+		ipnum, err := strconv.Atoi(strings.Split(ips, `.`)[3])
+		if err != nil {
+			log.Info(err)
+		}
+		return result, ipnum, ipnum
+	}
+	result := strings.Split(ips, `.`)[0] + "." + strings.Split(ips, `.`)[1] + "." + strings.Split(ips, `.`)[2] + "."
+	hostStartIP, err := strconv.Atoi(strings.Split(strings.Split(ips, ".")[len(strings.Split(ips, "."))-1], `-`)[0])
+	if err != nil {
+		log.Info(err)
+	}
+	hostStopIP, err := strconv.Atoi(strings.Split(strings.Split(ips, ".")[len(strings.Split(ips, "."))-1], `-`)[1])
+	if err != nil {
+		log.Info(err)
+	}
+	return result, hostStartIP, hostStopIP
+}
